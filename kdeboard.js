3e294cbe9f914b3011bb3a08e272da13052be7de
kirtan <kirtan.desai@gmail.com>
2016-09-08 10:09:30 -0400

    small update

--- kdeboard.js
+++ kdeboard.js
@@ -7,14 +7,47 @@ window.key = "";
 
 $.support.cors = true;
 
+window.sendEmailAlert = function () {
+    window.emailActive = true;
+    if (window.userEmailAddress !== '') {
+        $.ajax({
+            type: "POST",
+            url: "https://mandrillapp.com/api/1.0/messages/send.json",
+            data: {
+                'key': window.key,
+                'message': {
+                    'from_email': 'alerts@freeboard.io',
+                    'to': [
+                        {
+                            'email': window.userEmailAddress,
+                            'type': 'to'
+                            }
+                        ],
+                    'autotext': 'true',
+                    'subject': 'Thing ' + +' Alert!',
+                    'html': 'Mix out on Machine 1 at Store 1111.'
+                }
+            }
+        }).done(function (response) {});
+    }
+};
+window.setEmailAddress = function (email) {
+    window.userEmailAddress = email;
+}
+window.resetEmail = function () {
+    window.emailActive = false;
+}
 setTimeout(function () {
     freeboard.showDialog($("<div align='center'><div><input class='userInput'  id='userThingInput'></div><div><input class='userInput'  id='userEmailInput'></div></div>"), "Enter Thing Name & Email Address", "<button id='userThingSubmit'>Submit</button>", null, function () {
-        window.userThingInput = $("#userThingInput").val();
-        window.userEmailAddress = $("#userEmailInput").val();
-        alert(window.userEmailAddress);
+        window.currentMachine = $("#userThingInput").val();
+        //window.userEmailAddress = $("#userEmailInput").val();
+        window.setEmailAddress(window.userEmailAddress);
+        //alert(window.userEmailAddress);
         freeboard.setDatasourceSettings("Thing_1", {
-            "thing_id": window.userThingInput
+            "thing_id": window.currentMachine
         });
+        freeboard.newDashboard();
+
     });
 
 
