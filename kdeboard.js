bde2fb6f72b3b2a246cdd842f6dea8cfe45801bd
Kdebugg <kirtan.desai@gmail.com>
2016-09-14 08:52:50 -0400

    00

--- kdeboard.js
+++ kdeboard.js
@@ -109,9 +109,9 @@ setTimeout(function () {
         //window.userEmailAddress = $("#userEmailInput").val();
         window.setEmailAddress(window.userEmailAddress);
         //alert(window.userEmailAddress);
-//        freeboard.setDatasourceSettings("my_thing_1", {
-//            "thing_id": window.currentMachine
-//        });
+        freeboard.setDatasourceSettings("my_thing_1", {
+            "thing_id": window.currentMachine
+        });
 //        freeboard.setDatasourceSettings("Google Map", {
 //            "latitude": datasources["my_thing_1"]["your_latitude"],
 //            "longitude": datasources["my_thing_1"]["your_longitude"]
